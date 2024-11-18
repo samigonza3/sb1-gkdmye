6d7e43432773a5f727955bf124a201d373ac75d6
samigonza3 <samigonza3@gmail.com>
2024-11-17 21:03:15 -0500

    Update app

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import BlogCarousel from './components/BlogCarousel';
 import SubscribeForm from './components/SubscribeForm';
 import SocialLinks from './components/SocialLinks';
-import haroldSeguraImage from './assets/img/harold-segura.jpg';
+import haroldSeguraImage from './img/harold-segura.jpg';
 
 
 function App() {
